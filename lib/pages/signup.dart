1156c8d9b0873d1cae18ff82a6f57ff6cc597adb
Wahdu1 <113811762+Wahdu1@users.noreply.github.com>
2023-04-08 15:50:51 -0700

    anthony
    my stuff

---
+++ lib/pages/signup.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign Up'),
+      ),
+      body: Center(
+        child: Text('Sign Up Page'),
+      ),
+    );
+  }
+}
