a9405d6427e68a5559e3a97469db5d2f26ea201b
jevmors <jevmors@gmail.com>
2023-04-08 10:22:43 -0700

    initial project setup

---
+++ teaching_at_svhsp/README.md
@@ -0,0 +1,3 @@
+# teaching_at_svhsp
+
+An app containing all the educational material utilized in SVHSP for teaching purposes.
---
+++ teaching_at_svhsp/android/app/src/main/kotlin/com/example/teaching_at_svhsp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.teaching_at_svhsp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
