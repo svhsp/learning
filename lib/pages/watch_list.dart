b4810690b03569d34a7311a8a0ce46906308a208
Adam Yang <129695206+adamyhyang@users.noreply.github.com>
2023-04-29 12:09:46 -0700

    HW due for 4/29/2023
    Created loading page

--- lib/pages/watch_list.dart
+++ lib/pages/watch_list.dart
@@ -3,49 +3,41 @@ import 'package:flutter/material.dart';
 import '../models/stock_info.dart';
 import '../services/stock_fetcher.dart';
 
-class WatchlistPage extends StatefulWidget {
-  @override
-  _WatchlistPageState createState() => _WatchlistPageState();
-}
-
-class _WatchlistPageState extends State<WatchlistPage> {
-  List<StockInfo> _stocksInfo = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _fetchStocksInfo();
-  }
-
-  void _fetchStocksInfo() async {
-    final stocksInfo = await StockFetcher.fetchStocks(['GOOG', 'AAPL', 'TSLA']);
-    setState(() {
-      _stocksInfo = stocksInfo;
-    });
-  }
-
+class WatchlistPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Stock Watchlist'),
       ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: DataTable(
-          columns: [
-            DataColumn(label: Text('Symbol')),
-            DataColumn(label: Text('Price')),
-            DataColumn(label: Text('Change')),
-          ],
-          rows: _stocksInfo.map((stock) => DataRow(
-            cells: [
-              DataCell(Text(stock.symbol)),
-              DataCell(Text(stock.price.toStringAsFixed(2))),
-              DataCell(Text(stock.change.toStringAsFixed(2))),
-            ],
-          )).toList(),
-        ),
+      body: FutureBuilder<List<StockInfo>>(
+        future: StockFetcher.fetchStocks(['GOOG', 'AAPL', 'TSLA']),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            final stocksInfo = snapshot.data!;
+            return SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: DataTable(
+                columns: [
+                  DataColumn(label: Text('Symbol')),
+                  DataColumn(label: Text('Price')),
+                  DataColumn(label: Text('Change')),
+                ],
+                rows: stocksInfo.map((stock) => DataRow(
+                  cells: [
+                    DataCell(Text(stock.symbol)),
+                    DataCell(Text(stock.price.toStringAsFixed(2))),
+                    DataCell(Text(stock.change.toStringAsFixed(2))),
+                  ],
+                )).toList(),
+              ),
+            );
+          }
+        },
       ),
     );
   }
