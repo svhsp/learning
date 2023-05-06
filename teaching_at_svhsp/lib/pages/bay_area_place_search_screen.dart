cb237d4f6631de80eed353e7deb7ac00fe1edb95
jevmors <jevmors@gmail.com>
2023-05-06 10:27:05 -0700

    add list page for BayArea Place Search App

--- teaching_at_svhsp/lib/pages/bay_area_place_search_screen.dart
+++ teaching_at_svhsp/lib/pages/bay_area_place_search_screen.dart
@@ -3,6 +3,17 @@ import 'package:flutter/material.dart';
 
 class BayAreaPlaceSearchScreen extends StatefulWidget {
   static final String pageName = "/bay_area_place_search";
+  static final List<String> bayAreaPlaces = [
+    "Almaden",
+    "Los Gatos",
+    "Sunnyvale",
+    "Mountain View",
+    "San Francisco",
+    "Fremont",
+    "San Jose",
+    "Morgan Hill"
+  ];
+
   const BayAreaPlaceSearchScreen({Key? key}) : super(key: key);
 
   @override
@@ -22,7 +33,35 @@ class _BayAreaPlaceSearchScreenState extends State<BayAreaPlaceSearchScreen> {
             )),
       ),
       body: Column(
-        children: [],
+        children: [
+          Center(
+            child: ElevatedButton.icon(
+              icon: Icon(Icons.search, color: Colors.red),
+              label: Text("Search", style: TextStyle(color: Colors.red)),
+              onPressed: () => {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  side: BorderSide(color: Colors.blue)),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          ListView.builder(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  BayAreaPlaceSearchScreen.bayAreaPlaces.elementAt(index),
+                  style: TextStyle(fontSize: 15),
+                ),
+              );
+            },
+            itemCount: BayAreaPlaceSearchScreen.bayAreaPlaces.length,
+          )
+        ],
       ),
     );
   }
